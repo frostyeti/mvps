d4638984ee97aea930ab4ec0c322d2966bf8abfd
Grimfrost Mage <180069990+grimfrostmage@users.noreply.github.com>
2025-11-15 23:04:32 -0600

    dotnet - add bcl/Secrets

---
+++ dn/bcl/Secrets/src/ISecretVerifier.cs
@@ -0,0 +1,8 @@
+namespace FrostYeti;
+
+public interface ISecretVerifier
+{
+    bool Verify(ReadOnlySpan<char> secret, ReadOnlySpan<char> hash);
+
+    bool Verify(ReadOnlySpan<byte> secret, ReadOnlySpan<byte> hash);
+}
