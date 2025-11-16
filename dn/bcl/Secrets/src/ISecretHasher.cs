0a5d8686b09dafc1500d57ff98c306ca72e91dd8
Grimfrost Mage <180069990+grimfrostmage@users.noreply.github.com>
2025-11-15 23:04:32 -0600

    dotnet - add bcl/Secrets

---
+++ dn/bcl/Secrets/src/ISecretHasher.cs
@@ -0,0 +1,8 @@
+namespace FrostYeti.Secrets;
+
+public interface ISecretHasher
+{
+    ReadOnlySpan<byte> ComputeHash(ReadOnlySpan<char> secret);
+
+    ReadOnlySpan<byte> ComputeHash(ReadOnlySpan<byte> secret);
+}
